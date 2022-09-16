c6140204f218701ec69e3b4172200e686db7f2b9
Chris Umphlett <7210482+chrisumphlett@users.noreply.github.com>
2022-09-16 14:35:31 -0400

    Create get_webinar_qanda.R

---
+++ R/get_webinar_qanda.R
@@ -0,0 +1,39 @@
+#' Get Webinar Q & A
+#' 
+#' Get the Q&A summary from a single webinar.
+#' 
+#' @param webinar_id Zoom Webinar Id, typically an 11 digit number.
+#' @param account_id Account Id granted by the Zoom developer app.
+#' @param client_id Client Id granted by the Zoom developer app.
+#' @param client_secret Client secret granted by the Zoom developer app.
+#' 
+#' @seealso See <https://marketplace.zoom.us/docs/api-reference/zoom-api/> for 
+#' documentation on the Zoom API.
+#' @export
+#' @examples
+#' \dontrun{
+#' dat <- get_webinar_qanda(webinarID = "99911112222",
+#'   your_account_id,
+#'   your_client_id,
+#'   your_client_secret)
+#' }
+
+get_webinar_qanda <- function(webinar_id,
+                           account_id,
+                           client_id,
+                           client_secret)
+{
+  # Get new access token
+  access_token <- get_access_token(account_id, client_id, client_secret)
+  
+  # Function-specific API stuff
+  api_url <- generate_url(query = "getwebinarqanda",
+                          webinar_id = webinar_id)
+  
+  # Send GET request to specific survey
+  resp <- zoom_api_request(verb = "GET", url = api_url, token = access_token, query_params = "")
+  
+  df <- as.data.frame(jsonlite::fromJSON(resp, flatten = TRUE)) %>%
+    unnest(questions.question_details)
+  
+}
